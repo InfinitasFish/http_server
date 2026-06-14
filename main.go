567fef462ccd0d8408f4ea9e5b06d1bbbdd8ae0f
InfinitasFish <iq000012@gmail.com>
2026-06-14 23:31:47 +0500

    empty server

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+	)
+
 
 func main() {
 	fmt.Println("test")
+	mux := http.NewServeMux()
+	
+	srv := http.Server{
+		Addr: ":8080",
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		Handler: mux,
+	}
+
+	srv.ListenAndServe()
 }
+
