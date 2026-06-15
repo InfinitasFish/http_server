7e5cbbee9b3483b91ecc34f5ba9683d03642c188
InfinitasFish <iq000012@gmail.com>
2026-06-15 23:57:01 +0500

    endpoints handlers

--- main.go
+++ main.go
@@ -3,21 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 	)
 
 
 func main() {
-	fmt.Println("test")
-	mux := http.NewServeMux()
-	
-	srv := http.Server{
-		Addr: ":8080",
-		ReadTimeout:  15 * time.Second,
-		WriteTimeout: 15 * time.Second,
-		Handler: mux,
-	}
+	root := http.Dir(".")
+	fmt.Println(root)
 
-	srv.ListenAndServe()
+	// ServeMux is an HTTP request multiplexer. It matches the URL of each incoming request against a list 
+	// of registered patterns and calls the handler for the pattern that most closely matches the URL.
+	mux := http.NewServeMux()
+
+	// "index.html" is served from "/" by convention
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(root)))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		r.Header.Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	http.ListenAndServe(":8080", mux)
 }
 
