c696fa7977f57b8029ed65ab28c0ce72fa543b8e
InfinitasFish <iq000012@gmail.com>
2026-07-20 23:10:06 +0500

    JWT auth base

--- go.mod
+++ go.mod
@@ -3,13 +3,16 @@ module http_server
 go 1.25.5
 
 require internal/database v1.0.0
+
 require internal/auth v1.0.0
 
 replace internal/database => ./internal/database
+
 replace internal/auth => ./internal/auth
 
 require (
 	github.com/alexedwards/argon2id v1.0.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.12.3 // indirect
--- internal/auth/hashing.go
+++ internal/auth/hashing.go
@@ -2,8 +2,11 @@ package auth
 
 import (
 	"runtime"
+	"time"
 	"log"
 	"github.com/alexedwards/argon2id"
+	"github.com/google/uuid"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func HashPassword(password string) (string, error) {
@@ -32,3 +35,45 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 	return match, err
 }
 
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	jwToken := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		jwt.RegisteredClaims{
+			Issuer: "chirpy-access",
+			IssuedAt: jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+			Subject: userID.String(),
+		})
+
+	signed, err := jwToken.SignedString(tokenSecret)
+	if err != nil {
+		log.Println("Error while signing JWT token", err)
+		return "", err
+	}
+
+	return signed, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	jwToken, err := jwt.ParseWithClaims(
+		tokenString, 
+		&jwt.RegisteredClaims{},
+		// what key should I use to verify this token's signature?"
+		func(token *jwt.Token) (interface{}, error) {
+			return tokenSecret, nil
+		})
+
+	if err != nil {
+		log.Println("Error while parsing JWT token", err)
+		return uuid.Nil, err
+	}
+
+	claims, _ := jwToken.Claims.(jwt.RegisteredClaims)
+	userID, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		log.Println("Error while parsing user UUID", err)
+		return uuid.Nil, err
+	}
+
+	return userID, nil
+}
